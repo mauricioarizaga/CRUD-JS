9654a7b38b06e24fee07938fad5ffbad0c16cd8a
Mauricio Ariel Arizaga <59258066+mauricioarizaga@users.noreply.github.com>
2020-07-19 21:02:29 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 function db() {
-  return new Sequelize("postgres://postgres:postgres@localhost:5432/wescrud", {
+  return new Sequelize("postgres://postgres:postgres@localhost:5432/crud", {
     logging: console.log, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   });
